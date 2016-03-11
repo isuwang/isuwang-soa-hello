7436e07b5db89f64d126b90fc72ef661416171de
craneding <crane.ding@163.com>
2016-03-11 18:38:27 +0800

    优化

--- hello-client/src/main/java/com/isuwang/soa/hello/client/TestClient.java
+++ hello-client/src/main/java/com/isuwang/soa/hello/client/TestClient.java
@@ -3,19 +3,45 @@ package com.isuwang.soa.hello.client;
 import com.isuwang.soa.hello.HelloServiceClient;
 import org.apache.thrift.TException;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Created by tangliu on 2016/3/3.
  */
 public class TestClient {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int count = 50;
 
-        HelloServiceClient client = new HelloServiceClient();
-        try {
-            System.out.println(client.sayHello("LiLei"));
-        } catch (TException e) {
-            e.printStackTrace();
+        while (true) {
+            final CountDownLatch latch = new CountDownLatch(count);
+
+            newTask(count, latch);
+
+            latch.await();
+
+            Thread.sleep(2000);
         }
     }
+
+    private static void newTask(int count, CountDownLatch latch) {
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < count; i++) {
+                    HelloServiceClient client = new HelloServiceClient();
+                    try {
+                        System.out.println(client.sayHello("LiLei"));
+
+                        latch.countDown();
+                    } catch (TException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+
 }
